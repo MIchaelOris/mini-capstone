f920185b81113d9f16dbb3ab1092a705b5c8a404
MichaelOris <mikeyo197@gmail.com>
2018-01-18 21:37:42 -0600

    mini-capstone1-18

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,48 @@
 class ProductsController < ApplicationController
-  
-end
+
+    def index
+
+      products = Product.all
+      render json: products.as_json
+
+    end
+
+    def create 
+      product = Product.new(
+                          name: params[:name], 
+                          price: params[:price], 
+                          image_url: params[:image_url], 
+                          description: params[:description]
+                          )
+      product.save
+      render json: products.as_json
+
+    end 
+
+    def show
+      product = Product.find(params[:id])
+      render json: product.as_json
+
+    end 
+
+
+    def update 
+
+    product = Product.find(params[:id])
+    product.title = params[:name]
+    product.chef =params[:price]
+    product.ingredients =params[:image_url]
+    product.directions =params[:description]
+    product.save
+
+    render json: products.as_json
+
+    end 
+
+    def destroy
+      product = product.find(params[:id])
+      product.destroy
+      render json: {message: "Succesfully destroyed the Toy! ##{product.id}."}
+    end
+
+end 
